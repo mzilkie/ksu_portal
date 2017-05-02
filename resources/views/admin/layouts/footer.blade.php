a8bb1fd9b1ec7b7bdb615551ca8f88a51003c1a2
STC167User <STC167User@labs.salina.k-state.edu>
2017-05-02 14:11:43 -0500

    Created the new views for admin

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,13 +1,94 @@
+            <footer class="footer">
+                <div class="container-fluid">
+                    <nav class="pull-left">
+                        <ul>
+                            <li>
+                                <a href="{{route('home')}}">
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{route('/campaigns')}}">
+                                    Campaigns
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{route('/interactions')}}">
+                                    Interactions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{route('/tasks')}}">
+                                    Tasks
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{route('/manage')}}">
+                                    Manage Users
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <p class="copyright pull-right">
+                        &copy;
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        <a href="http://polytechnic.k-state.edu">Kansas State University - Polytechnic</a>
+                    </p>
+                </div>
+            </footer>
+        </div>
     </div>
-    <!-- /#wrapper -->
-
-
-<!-- Scripts -->
-<script src="{{ URL::asset('js/sidebar.js') }}"></script>
-<script src="{{ URL::asset('js/jquery.min.js') }}"></script>
-<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="{{ URL::asset('js/ie10-viewport-bug-workaround.js')}}"></script>
 </body>
+<!--   Core JS Files   -->
+<script src="/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="/js/jquery-ui.min.js" type="text/javascript"></script>
+<script src="/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/js/material.min.js" type="text/javascript"></script>
+<script src="/js/perfect-scrollbar.jquery.min.js" type="text/javascript"></script>
+<!-- Forms Validations Plugin -->
+<script src="/js/jquery.validate.min.js"></script>
+<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+<script src="/js/moment.min.js"></script>
+<!--  Charts Plugin -->
+<script src="/js/chartist.min.js"></script>
+<!--  Plugin for the Wizard -->
+<script src="/js/jquery.bootstrap-wizard.js"></script>
+<!--  Notifications Plugin    -->
+<script src="/js/bootstrap-notify.js"></script>
+<!-- DateTimePicker Plugin -->
+<script src="/js/bootstrap-datetimepicker.js"></script>
+<!-- Vector Map plugin -->
+<script src="/js/jquery-jvectormap.js"></script>
+<!-- Sliders Plugin -->
+<script src="/js/nouislider.min.js"></script>
+<!--  Google Maps Plugin    -->
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<!-- Select Plugin -->
+<script src="/js/jquery.select-bootstrap.js"></script>
+<!--  DataTables.net Plugin    -->
+<script src="/js/jquery.datatables.js"></script>
+<!-- Sweet Alert 2 plugin -->
+<script src="/js/sweetalert2.js"></script>
+<!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="/js/jasny-bootstrap.min.js"></script>
+<!--  Full Calendar Plugin    -->
+<script src="/js/fullcalendar.min.js"></script>
+<!-- TagsInput Plugin -->
+<script src="/js/jquery.tagsinput.js"></script>
+<!-- Material Dashboard javascript methods -->
+<script src="/js/material-dashboard.js"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="/js/demo.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        // Javascript method's body can be found in assets/js/demos.js
+        demo.initDashboardPageCharts();
+
+        demo.initVectorMap();
+    });
+</script>
 
 </html>
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,93 +1,192 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<meta name="Admissions, K-State, CRM, Portal, Prospective Students" content="">
-<meta name="Matthew Zilkie, Jacob Dehoff, Megan" content="">
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<link rel="icon" href="../../favicon.ico">
-
-<title>{{ config('app.name', 'Laravel') }}</title>
-<!-- Stylesheets -->
-<link href="{{ URL::asset('css/bootstrap.min.css')}}" rel="stylesheet">
-<link href="{{ URL::asset('css/app.css')}}" rel="stylesheet">
-<link href="{{ URL::asset('css/ie10-viewport-bug-workaround.css')}}" rel="stylesheet">
-
-<!-- scripts -->
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-<script>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png" />
+    <link rel="icon" type="image/png" href="/img/favicon.png" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="Admissions, K-State, CRM, Portal, Prospective Students" content="">
+    <meta name="Matthew Zilkie, Jacob Dehoff, Megan" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width" />
+    <!-- Bootstrap core CSS     -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <!--  Material Dashboard CSS    -->
+    <link href="/css/material-dashboard.css" rel="stylesheet" />
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" />
+    <script>
         window.Laravel = <?php echo json_encode([
                 'csrfToken' => csrf_token(),
         ]); ?>
-</script>
-
+    </script>
 </head>
 
 <body>
-<div id="wrapper">
-        <div class="overlay"></div>
-    
-        <!-- Sidebar -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
-            <ul class="nav sidebar-nav">
-                <!-- Authentication Links -->
-                @if (Auth::guard('admins')->user())
-                <li class="sidebar-brand">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ url('/') }}"><i class="fa fa-fw fa-home"></i> Home</a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-fw fa-plus"></i> Information Form <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="{{ url('/form') }}">Form</a></li>
-                    <li><a href="{{ url('/form/create') }}">New Form</a></li>
-                    <li><a href="{{ url('/form/{username}/edit')}}">Edit Form</a></li>
-                  </ul>
-                </li>
-                <li>
-                    <a href="{{ url('/messages') }}"><i class="fa fa-fw fa-file-o"></i> Messages</a>
-                </li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-fw fa-plus"></i> {{ Auth::guard('admins')->user()->name }} <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">View Profile</a></li>
-                    <li><a href="#">Edit Profile</a></li>
-                    <li><a href="#">Settings</a></li>
-                    <li>
-                        <a href="{{ url('/adminlogout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Logout
+    <div class="wrapper">
+        <div class="sidebar" data-active-color="rose" data-background-color="black" data-image="../assets/img/sidebar-1.jpg">
+            <!--
+        Tip 1: You can change the color of active element of the sidebar using: data-active-color="purple | blue | green | orange | red | rose"
+        Tip 2: you can also add an image using data-image tag
+        Tip 3: you can change the color of the sidebar with data-background-color="white | black"
+    -->
+            <div class="logo">
+                <a href="{{route('home')}}" class="simple-text">
+                    KSU Admissions Portal
+                </a>
+            </div>
+            <div class="logo logo-mini">
+                <a href="{{route('home')}}" class="simple-text">
+                    KSU
+                </a>
+            </div>
+            <div class="sidebar-wrapper">
+                <div class="user">
+                    <div class="photo">
+                        <!--<img src="{{URL::asset('/img/' + $user_img)}}" /> -->
+                    </div>
+                    <div class="info">
+                        <a data-toggle="collapse" href="#collapseExample" class="collapsed">
+                            {{ Auth::guard('admins')->user()->firstName + " " + Auth::guard('admins')->user()->lastName }}
+                            <b class="caret"></b>
                         </a>
-
-                        <form id="logout-form" action="{{ url('/sadmin/logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
+                        <div class="collapse" id="collapseExample">
+                            <ul class="nav">
+                                <li>
+                                    <a href="#">My Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#">Edit Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#">Settings</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <ul class="nav">
+                    <li class="active">
+                        <a href="{{route('home')}}">
+                            <i class="material-icons">dashboard</i>
+                            <p>Dashboard</p>
+                        </a>
                     </li>
-                  </ul>
-                </li>
-                @else
-                <li class="sidebar-brand">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ url('/admin/login') }}"><i class="fa fa-fw fa-file-o"></i> Login</a>
-                </li>
-                <li>
-                    <a href="{{ url('/admin/register') }}"><i class="fa fa-fw fa-file-o"></i> Register</a>
-                </li>
-                @endif
-            </ul>
-        </nav>
-        <!-- /#sidebar-wrapper -->
+                    <li>
+                        <a data-toggle="collapse" href="#pagesExamples">
+                            <i class="material-icons">image</i>
+                            <p>Campaigns
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="pagesExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="{{route('/campaigns')}}">View</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('campaigns.run')}}">Run</a>
+                                </li>
+                                <li>
+                                    <a href="{{route('campaigns.history')}}">History</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#componentsExamples">
+                            <i class="material-icons">apps</i>
+                            <p>Interactions
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="componentsExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="./components/buttons.html">Create</a>
+                                </li>
+                                <li>
+                                    <a href="./components/grid.html">View</a>
+                                </li>
+                                <li>
+                                    <a href="./components/panels.html">Edit</a>
+                                </li>
+                                <li>
+                                    <a href="./components/sweet-alert.html">History</a>
+                                </li>>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#formsExamples">
+                            <i class="material-icons">content_paste</i>
+                            <p>Tasks
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="formsExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="./forms/regular.html">Create</a>
+                                </li>
+                                <li>
+                                    <a href="./forms/extended.html">Edit</a>
+                                </li>
+                                <li>
+                                    <a href="./forms/validation.html">View</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#tablesExamples">
+                            <i class="material-icons">grid_on</i>
+                            <p>Users
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="tablesExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="./tables/regular.html">Add</a>
+                                </li>
+                                <li>
+                                    <a href="./tables/extended.html">Edit</a>
+                                </li>
+                                <li>
+                                    <a href="./tables/datatables.net.html">Remove</a>
+                                </li>
+                                <li>
+                                    <a href="./tables/datatables.net.html">View</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a data-toggle="collapse" href="#mapsExamples">
+                            <i class="material-icons">place</i>
+                            <p>Maps
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="mapsExamples">
+                            <ul class="nav">
+                                <li>
+                                    <a href="./maps/google.html">Google Maps</a>
+                                </li>
+                                <li>
+                                    <a href="./maps/fullscreen.html">Full Screen Map</a>
+                                </li>
+                                <li>
+                                    <a href="./maps/vector.html">Vector Map</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
